26f5af68fd7f97d19081c3fecc52733e6457a3f4
areebvohra <areebvohra@gmail.com>
2020-02-21 03:13:51 +0500

    registration screen added

---
+++ src/screens/RegisterScreen/index.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Background from '../../components/Background';
+import Logo from '../../components/Logo';
+import Header from '../../components/Header';
+import Button from '../../components/Button';
+import TextInput from '../../components/TextInput';
+import BackButton from '../../components/BackButton';
+import { theme } from '../../core/theme';
+import { emailValidator, passwordValidator, nameValidator } from '../../core/utils';
+
+export default class RegisterScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: { value: '', error: '' },
+            email: { value: '', error: '' },
+            password: { value: '', error: '' },
+        };
+    }
+
+    _onSignUpPressed = () => {
+        const { name, email, password } = this.state;
+
+        const nameError = nameValidator(name.value);
+        const emailError = emailValidator(email.value);
+        const passwordError = passwordValidator(password.value);
+
+        if (emailError || passwordError || nameError) {
+            this.setState({
+                name: { value: name.value, error: nameError },
+                email: { value: email.value, error: emailError },
+                password: { value: password.value, error: passwordError }
+            })
+            return;
+        }
+
+        // navigation.navigate('Dashboard');
+    };
+
+    render() {
+        const { name, email, password } = this.state;
+
+        return (
+            <Background>
+                <Logo />
+
+                <Header>Create Account</Header>
+
+                <TextInput
+                    label="Name"
+                    returnKeyType="next"
+                    value={name.value}
+                    onChangeText={text => this.setState({ name: { value: text, error: '' } })}
+                    error={!!name.error}
+                    errorText={name.error}
+                />
+
+                <TextInput
+                    label="Email"
+                    returnKeyType="next"
+                    value={email.value}
+                    onChangeText={text => this.setState({ email: { value: text, error: '' } })}
+                    error={!!email.error}
+                    errorText={email.error}
+                    autoCapitalize="none"
+                    autoCompleteType="email"
+                    textContentType="emailAddress"
+                    keyboardType="email-address"
+                />
+
+                <TextInput
+                    label="Password"
+                    returnKeyType="done"
+                    value={password.value}
+                    onChangeText={text => this.setState({ password: { value: text, error: '' } })}
+                    error={!!password.error}
+                    errorText={password.error}
+                    secureTextEntry
+                />
+
+                <Button mode="contained" onPress={this._onSignUpPressed} style={styles.button}>Sign Up</Button>
+
+                <View style={styles.row}>
+                    <Text style={styles.label}>Already have an account? </Text>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Login')} >
+                        <Text style={styles.link}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+            </Background>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    label: {
+        color: theme.colors.secondary,
+    },
+    button: {
+        marginTop: 24,
+    },
+    row: {
+        flexDirection: 'row',
+        marginTop: 4,
+    },
+    link: {
+        fontWeight: 'bold',
+        color: theme.colors.primary,
+    },
+});
