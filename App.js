e7471f8efc1ca77eb009b1dccf7194f1d00a3f9b
unknown <areebvohra@gmail.com>
2020-02-21 19:34:14 +0500

    code optamized

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default class App extends Component {
   constructor(props) {
@@ -8,6 +9,15 @@ export default class App extends Component {
     this.state = {
     };
   }
+
+  // componentDidMount = async () => {
+  //   const value = await AsyncStorage.getItem('@userID');
+  //   if (value !== null) {
+  //     console.log(value);
+  //   } else {
+  //     console.log("not present");
+  //   }
+  // }
 
   render() {
     StatusBar.setHidden(true)
