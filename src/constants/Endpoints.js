cce000699212484976de6ebb71c994a4651f50d0
unknown <areebvohra@gmail.com>
2020-02-25 21:00:21 +0500

    endpoints added

--- src/constants/Endpoints.js
+++ src/constants/Endpoints.js
@@ -3,6 +3,7 @@ const BaseURL = "http://192.168.0.34:3000"
 const EndPoints = {
     login: `${BaseURL}/api/login`,
     register: `${BaseURL}/api/register`,
+    userImage: `${BaseURL}/upload/`
 }
 
 export default EndPoints;
