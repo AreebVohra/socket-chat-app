047d17e3968fd076870c049bddb2983f42cf3c5e
unknown <areebvohra@gmail.com>
2020-02-28 17:51:05 +0500

    userImage link updated

--- src/constants/Endpoints.js
+++ src/constants/Endpoints.js
@@ -3,7 +3,7 @@ export const BaseURL = 'http://192.168.0.34:3000';
 export const Endpoints = {
     login: `${BaseURL}/api/login`,
     register: `${BaseURL}/api/register`,
-    userImage: `${BaseURL}/uploads/`
+    userImage: `${BaseURL}/uploads/avatars/`
 }
 
 // export default Endpoints;
