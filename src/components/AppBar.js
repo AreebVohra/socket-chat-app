097dc418c9379fa00a194ed061d020bf61c5bc21
Muhammad Areeb Vohra <34376767+AreebVohra@users.noreply.github.com>
2020-03-19 16:23:57 +0500

    functionality enhanced

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,16 +1,34 @@
 import * as React from 'react';
-import { Appbar } from 'react-native-paper';
+import { Appbar, Menu } from 'react-native-paper';
 
-const AppBar = ({ title, goBack, actions }) => {
+const AppBar = ({ title, goBack, actions, menuVisible, onDismiss, openMenu, logout }) => {
     return (
         <Appbar.Header style={{ backgroundColor: '#005f51' }}>
-            <Appbar.BackAction color={'#ffffff'} onPress={goBack} />
+            {
+                goBack === null
+                    ? null
+                    : <Appbar.BackAction color={'#ffffff'} onPress={goBack} />
+            }
             <Appbar.Content color={'#ffffff'} title={title} />
             {
-                actions.map((value, index) =>
-                    <Appbar.Action color={'#ffffff'} key={index} icon={value.icon} onPress={value.onPress} />
-                )
+                actions === null
+                    ? null
+                    : actions.map((value, index) =>
+                        <Appbar.Action color={'#ffffff'} key={index} icon={value.icon} onPress={value.onPress} />
+                    )
             }
+            <Menu
+                visible={menuVisible}
+                onDismiss={onDismiss}
+                anchor={
+                    <Appbar.Action
+                        color="white"
+                        icon="dots-vertical"
+                        onPress={openMenu}
+                    />
+                }>
+                <Menu.Item title="Logout" onPress={logout} />
+            </Menu>
         </Appbar.Header>
     )
 };
