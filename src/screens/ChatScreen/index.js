bb9451287033dab265b239579e769d6ace08b10a
Muhammad Areeb Vohra <34376767+AreebVohra@users.noreply.github.com>
2020-03-19 12:50:41 +0500

    header styling added

--- src/screens/ChatScreen/index.js
+++ src/screens/ChatScreen/index.js
@@ -8,14 +8,28 @@ import ChatBubbleWithReply from '../../components/ChatBubbleWithReply';
 import { Endpoints, BaseURL } from '../../constants/Endpoints';
 
 import io from 'socket.io-client';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import Feather from 'react-native-vector-icons/Feather'
 import ImagePicker from 'react-native-image-picker';
 import { GiftedChat, Bubble, Message, Day } from 'react-native-gifted-chat';
-import AppBar from '../../components/AppBar';
+import { HeaderButtons, HeaderButton, Item } from 'react-navigation-header-buttons';
+
+const IoniconsHeaderButton = passMeFurther => (
+  <HeaderButton {...passMeFurther} IconComponent={MaterialCommunityIcons} iconSize={20} color="white" />
+);
 
 const { width, height } = Dimensions.get('window');
 
 export default class ChatScreen extends Component {
+  static navigationOptions = {
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+        <Item title="reply" iconName="reply" />
+        <Item title="dots-vertical" iconName="dots-vertical" />
+      </HeaderButtons>
+    ),
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -143,16 +157,6 @@ export default class ChatScreen extends Component {
         resizeMode="cover"
         style={{ width, height: '100%' }}
         source={require('../../assets/background.png')}>
-        <AppBar
-          title={this.props.navigation.getParam('roomName')}
-          goBack={() => this.props.navigation.goBack()}
-          actions={
-            [
-              { icon: 'reply', onPress: this._action },
-              { icon: 'dots-vertical', onPress: this._action }
-            ]
-          }
-        />
         <GiftedChat
           messages={messages}
           onSend={this.onSend}
