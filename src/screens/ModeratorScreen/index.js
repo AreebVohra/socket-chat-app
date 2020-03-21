63b7e6109be7ac1367c4c2dfdd78b29796a086cc
areebvohra <areebvohra@gmail.com>
2020-03-22 02:25:23 +0500

    components added

--- src/screens/ModeratorScreen/index.js
+++ src/screens/ModeratorScreen/index.js
@@ -1,18 +1,88 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+
+import io from 'socket.io-client';
+import { Provider } from 'react-native-paper';
+
+import { BaseURL } from '../../constants/Endpoints';
+import AppBar from '../../components/AppBar';
 
 export default class ModeratorScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
         };
+
+        this.socket = io(BaseURL);
+        this.onRecieveRooms = this.onRecieveRooms.bind(this)
+        this.getRooms = this.getRooms.bind(this);
+        this.getRooms();
+        this.onRecieveRooms();
+    }
+
+    chatRoom = (chatID, roomName) => {
+        this.socket.emit('chatSelected', chatID);
+        this.props.navigation.navigate('Chat', { selected: chatID, roomName })
+    }
+
+    onRecieveRooms() {
+        this.socket.on('roomsAvailable', (rooms) => {
+            this.setState({
+                chatRoom: [...rooms]
+            })
+        })
+    }
+
+    getRooms() { this.socket.emit('roomsAvailable') }
+
+    componentWillUnmount() {
+        this.socket.removeEventListener('roomsAvailable')
     }
 
     render() {
         return (
-            <View>
-                <Text> Moderator Screen </Text>
-            </View>
+            <View style={styles.container}>
+                <Provider>
+                    <AppBar
+                        title={this.props.navigation.state.routeName}
+                        actions={null}
+                        goBack={null}
+                        {...this.props}
+                    />
+                    <FlatList
+                        style={{ width: '100%' }}
+                        data={this.state.chatRoom}
+                        keyExtractor={item => item._id}
+                        renderItem={({ item }) => (
+                            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                                <TouchableOpacity
+                                    style={[styles.roomButton, { backgroundColor: item.active ? 'white' : '#DCDCDC' }]}
+                                    disabled={!item.active}
+                                    onPress={() => this.chatRoom([item.chat_id], item.name)}
+                                >
+                                    <Text style={{ fontSize: 18 }}>{item.name}</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                    />
+
+                </Provider>
+            </View >
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%'
+    },
+    roomButton: {
+        borderWidth: 1,
+        borderColor: 'black',
+        paddingHorizontal: '15%',
+        paddingVertical: '5%',
+        borderRadius: 15,
+        marginVertical: '2%'
+    },
+})
