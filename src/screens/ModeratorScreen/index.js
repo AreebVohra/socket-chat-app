fc929c093b7b54328c7dc7f8a1d4b4af060747d7
areebvohra <areebvohra@gmail.com>
2020-03-22 06:22:28 +0500

    checkbox added

--- src/screens/ModeratorScreen/index.js
+++ src/screens/ModeratorScreen/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, CheckBox } from 'react-native';
 
 import io from 'socket.io-client';
 import { Provider } from 'react-native-paper';
@@ -11,6 +11,15 @@ export default class ModeratorScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            chatRoom: [
+                { "_id": "5e4ed622434db1126c53fe47", "active": true, "chat_id": 1, "name": "Room 1" },
+                { "_id": "5e4ef0fbbe8fbf126cebdb9f", "active": false, "chat_id": 2, "name": "Room 2" },
+                { "_id": "5e4ef10cbe8fbf126cebdba0", "active": true, "chat_id": 3, "name": "Room 3" },
+                { "_id": "5e767b2433ad712c7c5b641d", "active": false, "chat_id": 4, "name": "Room 4" },
+                { "_id": "5e767b2e33ad712c7c5b641e", "active": true, "chat_id": 5, "name": "Room 5" },
+                { "_id": "5e767b3933ad712c7c5b641f", "active": false, "chat_id": 6, "name": "Room 6" }
+            ],
+            checked: true
         };
 
         this.socket = io(BaseURL);
@@ -27,9 +36,9 @@ export default class ModeratorScreen extends Component {
 
     onRecieveRooms() {
         this.socket.on('roomsAvailable', (rooms) => {
-            this.setState({
-                chatRoom: [...rooms]
-            })
+            // this.setState({
+            //     chatRoom: [...rooms]
+            // })
         })
     }
 
@@ -37,6 +46,12 @@ export default class ModeratorScreen extends Component {
 
     componentWillUnmount() {
         this.socket.removeEventListener('roomsAvailable')
+    }
+
+    checkThisBox = (itemID) => {
+        let room = this.state.chatRoom;
+        room[itemID].active = !room[itemID].active
+        this.setState({ chatRoom: room })
     }
 
     render() {
@@ -53,7 +68,7 @@ export default class ModeratorScreen extends Component {
                         style={{ width: '100%' }}
                         data={this.state.chatRoom}
                         keyExtractor={item => item._id}
-                        renderItem={({ item }) => (
+                        renderItem={({ item, index }) => (
                             <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
                                 <TouchableOpacity
                                     style={[styles.roomButton, { backgroundColor: item.active ? 'white' : '#DCDCDC' }]}
@@ -65,7 +80,12 @@ export default class ModeratorScreen extends Component {
                             </View>
                         )}
                     />
-
+                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                        <CheckBox
+                            value={this.state.checked}
+                            onValueChange={() => this.setState({ checked: !this.state.checked })}
+                        />
+                    </View>
                 </Provider>
             </View >
         );
